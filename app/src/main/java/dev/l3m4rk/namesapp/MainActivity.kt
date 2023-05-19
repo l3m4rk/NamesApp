a37137c3b331b6f8095ea0c90856286ae34d1fea
Dmitry <b4r4n0v@gmail.com>
2023-05-19 15:01:36 +0300

    first commit

---
+++ app/src/main/java/dev/l3m4rk/namesapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.l3m4rk.namesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/dev/l3m4rk/namesapp/NamesApplication.kt
@@ -0,0 +1,7 @@
+package dev.l3m4rk.namesapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NamesApplication : Application()
